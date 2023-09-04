477b178b7e424bb598e1c9d9baee2ac02575652b
zeinmuhammad25 <55272694+zeinmuhammad25@users.noreply.github.com>
2023-09-04 17:25:01 +0700

    perubahan untuk import select

--- awalOtomasi/src/main/java/org/example/Main.java
+++ awalOtomasi/src/main/java/org/example/Main.java
@@ -5,6 +5,7 @@ import org.openqa.selenium.By;
 import org.openqa.selenium.WebDriver;
 import org.openqa.selenium.chrome.ChromeDriver;
 import org.openqa.selenium.chrome.ChromeOptions;
+import org.openqa.selenium.support.ui.Select;
 
 public class Main {
     public static void main (String[] args) throws InterruptedException {
